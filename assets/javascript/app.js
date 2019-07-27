92e4644f18c3c0282cf1c71562970c0805d6040e
LMBoyle <boylelukem@gmail.com>
2019-07-27 09:50:58 -0400

    added pseudocode in javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+// VARIABLES ===========================
+var correct = 0;
+var wrong = 0;
+var unanswered = 0;
+
+var triva = {
+  
+}
+// FUNCTIONS ===========================
+// TODO Display a start page
+// TODO On start, display first question
+// TODO Display and start timer
+// TODO If time is up, show time up screen
+// TODO If answer is wrong, show wrong answer screen
+// TODO If answer is wrong, add to wrong answers
+// TODO If answer is right, show right answer screen
+// TODO If answer is right, add to wrong answers
+// TODO After a few seconds, go to next question
+// TODO At end of all questions, show end screen and # of right/wrong
+// TODO If time up, show end screen and # of right/wrong/unanswered
+
+// CALL FUNCTIONS ======================
